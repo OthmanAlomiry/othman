a3ba2b1437206a4950dfd76c9df9b982d65e0b14
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-03-14 16:41:58 +0300

    Update b10.php

--- b10.php
+++ b10.php
@@ -1,10 +1,22 @@
 <?php
-// play.php
-$remote_url = "http://sportfet.shop/AD1/tracks-v1a1/mono.m3u8";
-
-// إرسال الترويسات المناسبة لملفات m3u8
-header('Content-Type: application/vnd.apple.mpegurl');
-
-// جلب وعرض المحتوى
-echo file_get_contents($remote_url);
+// تشفير الرابط بـ Base64 ليبقى بعيداً عن أعين المتصفح العادي
+$secret_link = base64_encode("http://sportfet.shop/AD1/tracks-v1a1/mono.m3u8");
 ?>
+
+<link href="https://vjs.zencdn.net/7.20.3/video-js.css" rel="stylesheet" />
+<script src="https://vjs.zencdn.net/7.20.3/video.min.js"></script>
+
+<video id="my-video" class="video-js vjs-default-skin" controls preload="auto" width="640" height="264">
+</video>
+
+<script>
+    // فك التشفير برمجياً وتشغيله
+    var encoded = "<?php echo $secret_link; ?>";
+    var decoded = atob(encoded); // تحويل Base64 إلى نص عادي
+    
+    var player = videojs('my-video');
+    player.src({
+        src: decoded,
+        type: 'application/x-mpegURL'
+    });
+</script>
