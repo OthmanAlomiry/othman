ea8a3c5f831ec787f06184a5cf44e2ceaa2bb222
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-03-15 11:00:10 +0300

    Create bs9.php

---
+++ bs9.php
@@ -0,0 +1,42 @@
+<?php
+
+$base = "http://135.125.109.73:9000/";
+$stream = $base . "beinsport9_.m3u8";
+
+header("Access-Control-Allow-Origin: *");
+
+if(isset($_GET['ts'])){
+
+    $ts = basename($_GET['ts']);
+    $url = $base . $ts;
+
+    header("Content-Type: video/mp2t");
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    echo $data;
+    exit;
+
+}
+
+header("Content-Type: application/vnd.apple.mpegurl");
+
+$ch = curl_init($stream);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+
+$m3u8 = curl_exec($ch);
+curl_close($ch);
+
+$m3u8 = preg_replace('/(.*\.ts)/', 'bs9.php?ts=$1', $m3u8);
+
+echo $m3u8;
+
+?>
