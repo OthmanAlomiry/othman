22f1f78fda40d2da8ce053d5d296577fba7b9520
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-03-18 10:39:29 +0300

    Update zz.php

--- zz.php
+++ zz.php
@@ -1,7 +1,7 @@
 <div style="max-width:800px; margin:20px auto; background:#000; border-radius:12px; overflow:hidden; box-shadow: 0 10px 30px rgba(0,0,0,0.5);">
   <div style="position:relative; padding-bottom:56.25%; height:0;">
     <iframe 
-      src="https://cdn-live.tv/api/v1/channels/player/?name=beIN%20SPORTS%205&code=sa&user=cdnlivetv&plan=free" 
+      src="https://cdn-live.tv/api/v1/channels/player/?name=beIN%20SPORTS%206&code=sa&user=cdnlivetv&plan=free" 
       style="position:absolute; top:0; left:0; width:100%; height:100%; border:0;" 
       allowfullscreen 
       allow="autoplay; encrypted-media; picture-in-picture"
