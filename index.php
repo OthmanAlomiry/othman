5eec6b28663259a4a08031908fb769bef442af63
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-03-24 03:16:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -62,6 +62,9 @@ date_default_timezone_set('Asia/Riyadh');
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>بوابة الرياضة - متجر الخدمة الرقمية</title>
     
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2760895204432673"
+     crossorigin="anonymous"></script>
+
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
