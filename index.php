5a0a5774e8b323e596de049c040ba3148df1a800
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-04-12 22:54:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(0); // يمكنك تغييره إلى E_ALL عند التجرب�
 date_default_timezone_set('Asia/Riyadh');
 $date_get = date('Y-m-d');
 
-$FOOTBALL_API_KEY_NEW = '273aaeb61360452588653ffea820cc19'; 
+$FOOTBALL_API_KEY_NEW = '7a80e065b1734a50806dfeadb888b4f5'; 
 $url_new = 'https://api.football-data.org/v4/matches';
 
 $leagues_map_new = [
