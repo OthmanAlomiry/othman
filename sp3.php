78fe16fef569365434fc9858783a748f4e5cefb3
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-04-24 10:25:01 +0300

    Update sp3.php

--- sp3.php
+++ sp3.php
@@ -18,7 +18,7 @@
     (function() {
         const videoElement = document.getElementById('live-player');
         // الرابط الذي أكدت أنه يعمل
-        const streamUrl = 'https://ssserver.site/SportTv-3/tracks-v1a1/mono.m3u8';
+        const streamUrl = 'https://ssserver.site/SportTV-4/tracks-v1a1/mono.m3u8';
 
         if (Hls.isSupported()) {
             const hls = new Hls({
