83a0ba2fd330f482e0925a98c290f17b690c96df
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-04-27 14:47:57 +0300

    Update b1.php

--- b1.php
+++ b1.php
@@ -1,6 +1,6 @@
 <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
     <iframe 
-        src="https://sportapps.elwady.dev/dl_player?id=beINAR1" 
+        src="https://pub-988290a10bbf42009d966bd9ef7153c8.r2.dev/1.m3u8id=beINAR1" 
         style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;" 
         allowfullscreen="true" 
         webkitallowfullscreen="true" 
