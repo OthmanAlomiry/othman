ff661608837c219f942fe2b013a2fa4a41e90404
OthmanAlomiry <39161600+OthmanAlomiry@users.noreply.github.com>
2026-04-29 13:37:23 +0300

    Update star2.php

--- star2.php
+++ star2.php
@@ -18,7 +18,7 @@
     (function() {
         const videoElement = document.getElementById('live-player');
         // الرابط الذي أكدت أنه يعمل
-        const streamUrl = 'https://ssserver.site/STC-SPORT2/tracks-v1a1/mono.m3u8';
+        const streamUrl = 'https://ssserver.site/AdSPORT1/tracks-v1a1/mono.m3u8';
 
         if (Hls.isSupported()) {
             const hls = new Hls({
